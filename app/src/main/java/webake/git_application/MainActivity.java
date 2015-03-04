6a21dfabbcd2b48ca97cee5953a8e4e3fd51b40e
sorfila <sorfila@gmail.com>
2015-03-04 11:36:53 +0100

    first commit

--- app/src/main/java/webake/git_application/MainActivity.java
+++ app/src/main/java/webake/git_application/MainActivity.java
@@ -8,6 +8,9 @@ import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 
+    private String cadena;
+    private Boolean boo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
